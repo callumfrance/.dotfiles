72850e62b27f6826a2e48a8131e8b5d822aaf2fe
Callum France <callum.france@healthengine.com.au>
2020-12-18 09:55:41 +0800

    commenting out git_fresh

--- .git_fresh.sh
+++ .git_fresh.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+################################################################################
+# Git Fresh - get the latest custom/master/main branch of a git repository
+################################################################################
+
+# function git_is_in_local() 
+#   Returns truthy if the provided branch name is already checked out locally
+#   Returns falsy otherwise
 function git_is_in_local() {
     local branch=${1}
     local existed_in_local=$(git branch --list ${branch})
@@ -13,6 +20,9 @@ function git_is_in_local() {
     fi
 }
 
+# function git_is_in_remote() 
+#   Returns truthy if the provided branch name exists in the remote
+#   Returns falsy otherwise
 function git_is_in_remote() {
     local branch=${1}
     local existed_in_remote=$(git ls-remote --heads origin ${branch})
@@ -26,6 +36,9 @@ function git_is_in_remote() {
     fi
 }
 
+# function git_check_me_out() 
+#   Returns truthy once the remote branch has been updated locally
+#   Returns falsy if the remote branch was not found
 function git_check_me_out() {
     local branch=${1}
     if git_is_in_remote $branch; then 
@@ -37,6 +50,9 @@ function git_check_me_out() {
     fi
 }
 
+# function git_fresh() 
+#   Tries to get an updated branch of <CUSTOM> or 'master' and then 'main'
+#       It executes in that order, finishing if it was able to complete one
 function git_fresh() {
     local branch_name=${1:-master} # set branch name to parameter of defaults to 'master'
     if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
